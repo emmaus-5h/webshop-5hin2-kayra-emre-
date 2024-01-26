f38cda516c25e226b13764c6f29dcf8c699d4a73
EmreMertArikan <121105450+EmreMertArikan@users.noreply.github.com>
2024-01-26 13:28:44 +0000

    Fouten weg gewerkt

--- db/create.sql
+++ db/create.sql
@@ -11,8 +11,8 @@ CREATE TABLE products (
   stock_id INTEGER,
   interest_id INTEGER,
   time_id INTEGER,
-  category VARCHAR (250)
-  doors INTEGER, 
+  category VARCHAR (250),
+  doors INTEGER 
 );
 
 
@@ -51,11 +51,11 @@ CREATE table category (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price,stock_id, interest_id, time_id, category) values ('Mercedez Benz', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 37000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('BMW', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 45000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Audi', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 33000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Toyota', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 19000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Ferrari', A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Mercedez Benz', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 'appp', 'appp','appp', 'appp', 37000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('BMW', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 'appp', 'appp', 'appp', 'appp', '077030122-3', 45000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Audi', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 'appp', 'appp', 'appp',  'appp', 33000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Toyota', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 'appp', 'appp', 'appp', 'appp', '693155505-7', 19000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Ferrari', 'Almost a Bride', 'appp', 'appp', 'appp', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
 --stocks
