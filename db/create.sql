1af726b8abd6e0f213f0ef3fd07139838ba3c016
KayraEkinci <92523573+KayraEkinci@users.noreply.github.com>
2024-02-02 13:26:48 +0000

    db bewerkt en accessoires toegevoegd + n:m relatie

--- db/create.sql
+++ db/create.sql
@@ -19,7 +19,8 @@ CREATE TABLE products (
   interest_id INTEGER,
   time_id INTEGER,
   category VARCHAR (250),
-  doors INTEGER 
+  doors INTEGER,
+  accessoires VARCHAR (250)
 );
 
 
@@ -33,19 +34,29 @@ CREATE table stocks (
 --interest 1:n relatie
 CREATE table interest (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  instock VARCHAR(150)
+  interest VARCHAR(150)
 );
 --time 1:n relatie
 CREATE table time (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  instock VARCHAR(150)
+  time VARCHAR(150)
 );
 --category 1:n relatie
 CREATE table category (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  instock VARCHAR(150)
+  category VARCHAR(150)
 );
 
+--accessoires n:m relatie 
+CREATE table accessoires (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+  accessoires VARCHAR(150)
+);
+  
+
+
+
+  
 
 
 
@@ -58,32 +69,38 @@ CREATE table category (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Mercedez Benz', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 'appp', 'appp','appp', 'appp', 37000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('BMW', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 'appp', 'appp', 'appp', 'appp', '077030122-3', 45000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Audi', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 'appp', 'appp', 'appp',  'appp', 33000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Toyota', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 'appp', 'appp', 'appp', 'appp', '693155505-7', 19000);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category) values ('Ferrari', 'Almost a Bride', 'appp', 'appp', 'appp', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedez Benz', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 'appp', 'appp','appp', 'appp', 'appp',  37000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', 'appp', 'appp', 'appp', 'appp', '077030122-3', 'appp', 45000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 'appp', 'appp', 'appp',  'appp', 'appp', 33000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Toyota', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 'appp', 'appp', 'appp', 'appp', '693155505-7',  'appp',19000);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari', 'Almost a Bride', 'appp', 'appp', 'appp', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 'appp', 14);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 'appp', 'appp', 'appp', 'appp',  'appp',14);
 
 --stocks
   insert into stocks (instock) values ('In Stock');
   insert into stocks (instock) values ('Out of stock');
   
 --interest 
-  insert into interest (name) values ('Best seller');
-  insert into interest (name) values ('New');
-  insert into interest (name) values ('DISCOUNT');
+  insert into interest (interest) values ('Best seller');
+  insert into interest (interest) values ('New');
+  insert into interest (interest) values ('DISCOUNT');
 
 --time
-  insert into time (name) values ('Regular, 3 months');
-  insert into time (name) values ('Fast Service, 6 weeks');
+  insert into time (time) values ('Regular, 3 months');
+  insert into time (time) values ('Fast Service, 6 weeks');
 
 -- category
-  insert into category (name) values ('SUV');
-  insert into category (name) values ('Sedan');
-  insert into category (name) values ('Coupé');
-  insert into category (name) values ('Station Wagon');
-  insert into category (name) values ('Supercar');
-  insert into category (name) values ('Sport');
+  insert into category (category) values ('SUV');
+  insert into category (category) values ('Sedan');
+  insert into category (category) values ('Coupé');
+  insert into category (category) values ('Station Wagon');
+  insert into category (category) values ('Supercar');
+  insert into category (category) values ('Sport');
+
+-- accesoires
+  insert into accessoires (accessoires) values ('Air conditioning');
+  insert into accessoires (accessoires) values ('Navigation');
+  insert into accessoires (accessoires) values ('Airbag');
+
 
 
