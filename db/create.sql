aabace1d1be615b41c2eb9e0fd366d8dd9a421ee
EmreMertArikan <121105450+EmreMertArikan@users.noreply.github.com>
2024-02-14 19:13:05 +0000

    fotos goed gezet. db goed aangevuld.

--- db/create.sql
+++ db/create.sql
@@ -68,26 +68,26 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 --Mercedez Benz
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedez Benz', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 37000, 1, 2, 1, 1, 2);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedes', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 45000, 1, 3, 1, 5, 3);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedes', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 33000, 2, 1, 2, 3, 1 );
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedez Benz', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 30000, 1, 2, 1, 1, 2);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedes AMG GT', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 240000, 1, 3, 1, 5, 3);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Mercedes CLA 250 4Matic', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-6', 77000, 2, 1, 2, 3, 1 );
 
 
 --BMW
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW M5 Competition', 'Dit is de nieuwste BMW.', '816905633-0', 180.000, 1, 2, 1, 1, 2);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW X6', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 120.000 ,1, 3, 1, 5 , 3);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW M3 Touring', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 110000, 2, 1, 2, 3, 1);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW M5 Competition', 'Met een maximaal vermogen van 460 kW (625 pk) en een op prestaties gerichte onderstelafstemming overtreft de BMW M5 Competition met M xDrive de hoogste verwachtingen. De high-performance sportsedan onderstreept dit visueel met talrijke designkenmerken in hoogglans zwart. De BMW M5 beschikt niet alleen over het op maximale tractie en dynamiek afgestelde M xDrive, maar ook over een onderstel dat sportief comfort op lange afstanden biedt. Samen met de 441 kW (600 pk) sterke 8-cilinder M motor ontstaat een met kracht en wendbaarheid doordrenkte rijdynamiek.', '847392056-1', 180000, 1, 2, 1, 1, 2);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW X6', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '274926706-9', 120000 ,1, 3, 1, 5 , 3);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('BMW M3 Touring', 'Pellentesque at nulla. Suspendisse potenti.', '620673298-5', 110000, 2, 1, 2, 3, 1);
 
 --Audi
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 120.000 ,1, 3, 1, 5 , 3);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '816905633-0', 89000, 2, 1, 2, 3, 1);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 96000, 1, 2, 1, 1, 2);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi RS6', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '206873206-8', 210000 ,1, 3, 1, 5 , 3);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi RSQ8', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '564376537-3', 220000, 2, 1, 2, 3, 1);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Audi A4', 'Pellentesque at nulla. Suspendisse potenti.', '947869476-9', 56000, 1, 2, 1, 1, 2);
 
 
 --Ferrari
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 120.000 ,1, 3, 1, 5 , 3);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 89000, 2, 1, 2, 3, 1);
-insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 96000, 1, 2, 1, 1, 2);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari SF90 Stradale', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '344769308-4', 800000 ,1, 3, 1, 5 , 3);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari 812 Superfast', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '655463827-7', 390000, 2, 1, 2, 3, 1);
+insert into products (name, description, code, price, stock_id, interest_id, time_id, category, accessoires) values ('Ferrari 488 Pista', 'Pellentesque at nulla. Suspendisse potenti.', '648967489-1', 99000, 1, 2, 1, 1, 2);
 
 
 --stocks
